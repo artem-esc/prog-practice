7410334c33f750b2038dc7b0a936e8ef37c4c72d
Artem <113693402+artem-esc@users.noreply.github.com>
2022-09-26 14:28:43 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        if (Console.CapsLock)
+        {
+            Console.WriteLine(1);
+        }
+        Console.WriteLine(Console.CapsLock);
+    }
+}
+
